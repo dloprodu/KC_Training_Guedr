8c2c4cf0991c15e5f0955f5477bf262409c1f78c
David Lopez Rodriguez <dloprodu@gmail.com>
2018-05-07 22:51:59 +0100

    View Layout

--- app/src/main/java/io/keepcoding/guedrbootamp6/MainActivity.kt
+++ app/src/main/java/io/keepcoding/guedrbootamp6/MainActivity.kt
@@ -2,11 +2,22 @@ package io.keepcoding.guedrbootamp6
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val numero = savedInstanceState?.getInt("Numero")
+
+        Log.v("UnTag","Han llamado a onCreate" )
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?, outPersistentState: PersistableBundle?) {
+        super.onSaveInstanceState(outState, outPersistentState)
+        outState?.putInt("Numero", 3)
     }
 }
