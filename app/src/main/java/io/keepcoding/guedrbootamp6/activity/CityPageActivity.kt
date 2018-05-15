eeee7846099c821fe7aab763ecacea47f67ca3fd
David Lopez Rodriguez <dloprodu@gmail.com>
2018-05-15 20:58:23 +0100

    view pager and custom toolbar

--- app/src/main/java/io/keepcoding/guedrbootamp6/activity/CityPageActivity.kt
+++ app/src/main/java/io/keepcoding/guedrbootamp6/activity/CityPageActivity.kt
@@ -4,6 +4,8 @@ import android.app.Fragment
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v13.app.FragmentPagerAdapter
+import android.view.Menu
+import android.view.MenuItem
 import io.keepcoding.guedrbootamp6.R
 import io.keepcoding.guedrbootamp6.fragment.ForecastFragment
 import io.keepcoding.guedrbootamp6.model.Cities
@@ -14,6 +16,9 @@ class CityPageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_city_page)
+
+        toolbar.setLogo(R.mipmap.ic_launcher)
+        setSupportActionBar(toolbar)
 
         val cities = Cities()
 
@@ -30,5 +35,24 @@ class CityPageActivity : AppCompatActivity() {
         }
 
         view_pager.adapter = adapter
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+
+        menuInflater.inflate(R.menu.pager_navigation, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean = when (item?.itemId) {
+        R.id.previous -> {
+            view_pager.currentItem = view_pager.currentItem - 1
+            true
+        }
+        R.id.next -> {
+            view_pager.currentItem = view_pager.currentItem + 1
+            true
+        }
+        else -> super.onOptionsItemSelected(item)
     }
 }
